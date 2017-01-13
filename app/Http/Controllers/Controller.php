3f518a0b4b16494534a94c76176a6d0c5752a7b4
vagrant <vagrant@homestead>
2017-01-13 13:41:15 +0000

    renomear app

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace finance\Http\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
--- config/app.php
+++ config/app.php
@@ -172,11 +172,11 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
+        finance\Providers\AppServiceProvider::class,
+        finance\Providers\AuthServiceProvider::class,
+        // finance\Providers\BroadcastServiceProvider::class,
+        finance\Providers\EventServiceProvider::class,
+        finance\Providers\RouteServiceProvider::class,
 
     ],
 
